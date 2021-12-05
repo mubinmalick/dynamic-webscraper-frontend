d6cd4b1d0c4b4a89795dbe83e6e45ffa9fdabb42
Mubin Malick <mubinmalick@gmail.com>
2021-12-06 02:07:38 +0500

    first commit

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./homepage.css"
+import { useHistory } from "react-router-dom";
+
+const Homepage = ({setLoginUser}) => {
+    const history = useHistory()
+    return (
+        
+        <div className="homepage"> 
+        <h1>Main Dashboard</h1>
+         <div className="button" onClick={() => history.push("/login")}> Logout</div>
+        </div>
+    )
+}
+
+export default Homepage;
